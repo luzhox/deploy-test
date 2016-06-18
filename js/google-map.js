0780634712bc73f87312a86d99b922696904517c
Luzhox <luis.moralesponce@gmail.com>
2016-06-18 00:01:19 -0500

    Subiendo Mapa

---
+++ js/google-map.js
@@ -0,0 +1,25 @@
+$(document).ready(CrearMapa);
+	var map;
+	var marcador;
+
+function CrearMapa() {
+	var image = new google.maps.MarkerImage('http://servitech.pe/images/location.png', new google.maps.Size(47,75));
+	var mapOptions = {
+		center: new google.maps.LatLng(-12.0540695,-77.0730754),
+		zoom: 17,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+	map = new google.maps.Map(document.getElementById("google-map"),mapOptions);
+	var place =  new google.maps.LatLng(-12.0540695,-77.0730754);
+	marcador = new google.maps.Marker({
+			position: place,
+			title:"Mi Casa",
+			map: map,
+			icon: image
+
+	});
+
+
+		
+
+}
